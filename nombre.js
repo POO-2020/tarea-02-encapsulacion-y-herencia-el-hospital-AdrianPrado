da8cf5471ef091d91e41df79c77281ff465f7452
Macbook Air <macbookair@MacBook-Air-de-Macbook.local>
2020-03-18 13:52:24 -0600

    Finish

---
+++ nombre.js
@@ -0,0 +1,23 @@
+export default class Nombre{
+  constructor(nombre,apellidoPaterno,apellidoMaterno){
+      this._nombre = nombre
+      this._apellidoPaterno = apellidoPaterno
+      this._apellidoMaterno = apellidoMaterno
+  }
+
+  getNombreCompleto(){
+      return (`${this._nombre} ${this._apellidoPaterno} ${this._apellidoMaterno}`)
+  }
+
+  getApellidoNombre(){
+      return (`${this._apellidoPaterno} ${this._apellidoMaterno} ${this._nombre}`)
+  }
+
+  getIniciales(){
+      return  (`${this._nombre.charAt(0)}${this._apellidoPaterno.charAt(0)}${this._apellidoMaterno.charAt(0)}`)
+  }
+
+  getApellidoPaterno(){
+      return this._apellidoPaterno
+  }
+}
